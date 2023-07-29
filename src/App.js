509069917a3a8ddecbaa028fb7dc9b44e955ca43
Shawn Kittel <shawn.m.kittel@gmail.com>
2023-07-29 09:10:46 -0600

    add route UpdateBlogPost

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import './App.css';
 import CreateBlogPost from './components/CreateBlogPost';
 import ShowBookList from './components/ShowBlogPostList';
 import ShowBlogPostList from './components/ShowBlogPostList';
+import ShowBlogPostDetails from './components/ShowBlogPostDetails';
+
 import ShowBookDetails from './components/ShowBookDetails';
 import UpdateBookInfo from './components/UpdateBookInfo';
+import UpdateBlogPost from './components/UpdateBlogPost';
 
 const App = () => {
   return (
     <Router>
       <div>
         <Routes>
-          <Route exact path='/' element={<ShowBookList />} />
+          <Route exact path='/' element={<ShowBlogPostList />} />
           <Route path='/create-blog-post' element={<CreateBlogPost />} />
-          <Route path='/edit-book:id' element={<UpdateBookInfo />} />
-          <Route
-            path='/show-blog-posts/:id'
-            element={<ShowBlogPostDetails />}
-          />
+          <Route path='/edit-book/:id' element={<UpdateBookInfo />} />
+          <Route path='/update-blog-post/:id' element={<UpdateBlogPost />} />
+          <Route path='/show-blog-post/:id' element={<ShowBlogPostDetails />} />
         </Routes>
       </div>
     </Router>
