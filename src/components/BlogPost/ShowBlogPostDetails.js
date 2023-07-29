72970cb067c38751f2ecdedb939cc4aeb50c3201
Shawn Kittel <shawn.m.kittel@gmail.com>
2023-07-29 09:58:24 -0600

    move files into respective folders

--- src/components/BlogPost/ShowBlogPostDetails.js
+++ src/components/BlogPost/ShowBlogPostDetails.js
@@ -4,7 +4,7 @@ import '../App.css';
 import axios from 'axios';
 
 function ShowBlogPostDetails(props) {
-  const [blogPostDetails, setBlogPostDetails] = useState({});
+  const [blogPost, setBlogPost] = useState({});
 
   const { id } = useParams();
   const navigate = useNavigate();
@@ -13,7 +13,7 @@ function ShowBlogPostDetails(props) {
     axios
       .get(`http://localhost:3001/api/blogposts/${id}`)
       .then((res) => {
-        setBlogPostDetails(res.data);
+        setBlogPost(res.data);
       })
       .catch((err) => {
         console.log('Error from ShowBlogPostDetails');
@@ -27,43 +27,43 @@ function ShowBlogPostDetails(props) {
         navigate('/');
       })
       .catch((err) => {
-        console.log('Error form ShowBlogPostDetails_deleteClick');
+        console.log('Error form ShowBlogPost_deleteClick');
       });
   };
 
-  const blogPostDetailsItem = (
+  const BlogPostItem = (
     <div>
       <table className='table table-hover table-dark'>
         <tbody>
           <tr>
             <th scope='row'>1</th>
             <td>Title</td>
-            <td>{blogPostDetails.title}</td>
+            <td>{blogPost.title}</td>
           </tr>
           <tr>
             <th scope='row'>2</th>
             <td>Author</td>
-            <td>{blogPostDetails.author}</td>
+            <td>{blogPost.author}</td>
           </tr>
           <tr>
             <th scope='row'>3</th>
             <td>ISBN</td>
-            <td>{blogPostDetails.isbn}</td>
+            <td>{blogPost.isbn}</td>
           </tr>
           <tr>
             <th scope='row'>4</th>
             <td>Publisher</td>
-            <td>{blogPostDetails.publisher}</td>
+            <td>{blogPost.publisher}</td>
           </tr>
           <tr>
             <th scope='row'>5</th>
             <td>Published Date</td>
-            <td>{blogPostDetails.published_date}</td>
+            <td>{blogPost.published_date}</td>
           </tr>
           <tr>
             <th scope='row'>6</th>
             <td>Description</td>
-            <td>{blogPostDetails.description}</td>
+            <td>{blogPost.description}</td>
           </tr>
         </tbody>
       </table>
@@ -77,33 +77,33 @@ function ShowBlogPostDetails(props) {
           <div className='col-md-10 m-auto'>
             <br /> <br />
             <Link to='/' className='btn btn-outline-warning float-left'>
-              Show blogPostDetails List
+              Show blogPost List
             </Link>
           </div>
           <br />
           <div className='col-md-8 m-auto'>
-            <h1 className='display-4 text-center'>blogPostDetails's Record</h1>
-            <p className='lead text-center'>View blogPostDetails's Info</p>
+            <h1 className='display-4 text-center'>blogPost's Record</h1>
+            <p className='lead text-center'>View blogPost's Info</p>
             <hr /> <br />
           </div>
-          <div className='col-md-10 m-auto'>{blogPostDetailsItem}</div>
+          <div className='col-md-10 m-auto'>{BlogPostItem}</div>
           <div className='col-md-6 m-auto'>
             <button
               type='button'
               className='btn btn-outline-danger btn-lg btn-block'
               onClick={() => {
-                onDeleteClick(blogPostDetails._id);
+                onDeleteClick(blogPost._id);
               }}
             >
-              Delete blogPostDetails
+              Delete blogPost
             </button>
           </div>
           <div className='col-md-6 m-auto'>
             <Link
-              to={`/update-blog-post/${blogPostDetails._id}`}
+              to={`/update-blog-post/${blogPost._id}`}
               className='btn btn-outline-info btn-lg btn-block'
             >
-              Edit blogPostDetails
+              Edit blogPost
             </Link>
           </div>
         </div>
