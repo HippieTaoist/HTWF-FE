afca4ccdd53854fc0d524ecec00f0be43173e215
Shawn Kittel <shawn.m.kittel@gmail.com>
2023-08-01 08:38:06 -0600

    minor change

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,7 +29,7 @@ function Home() {
         ));
 
   return (
-    <div className='ShowBlogPostList'>
+    <div className='Home'>
       <div className='container'>
         <div className='row'>
           <div className='col-md-12'>
