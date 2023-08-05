f66357ce57d83443c90930203b20f4f61817194a
Shawn Kittel <shawn.m.kittel@gmail.com>
2023-08-05 07:22:45 -0600

    developing navbar added file

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <nav>
+      Navigation
+      <nav>
+        <a href='#'>Books</a>
+        <a href='#'>Blog</a>
+        <a href='#'>Husbandry</a>
+        <a href='#'>Jokes and Memes</a>
+        <a href='#'>Products</a>
+        <a href='#'>Research</a>
+        <a href='#'>Links</a>
+      </nav>
+    </nav>
+  );
+}
